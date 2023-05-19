75c7be2f75100959920b26f6926fe83575a05903
simonkim1 <sevencuter@gmail.com>
2023-05-19 23:54:47 +0900

    프로그래머스_Day2

---
+++ Level 0/Python/문자열 겹쳐쓰기.py
@@ -0,0 +1,5 @@
+def solution(my_string, overwrite_string, s):
+    my_string = list(my_string)
+    for i in range(len(overwrite_string)):
+        my_string[s + i] = overwrite_string[i]
+    return ''.join(my_string)
