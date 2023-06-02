65487146330b6adb4087bc61fd5c5fb09450dfaf
simonkim1 <sevencuter@gmail.com>
2023-06-02 16:48:53 +0900

    프로그래머스_Day6

--- Level 0/Python/수열과 구간 쿼리 2.py
+++ Level 0/Python/수열과 구간 쿼리 2.py
@@ -4,13 +4,17 @@ def solution(arr, queries):
     for s, e, k in queries:
         tempArr = []
         
-        for i in sorted(arr[s:e + 1]):
-            if i > k:
-                tempArr.append(i)
+#         for i in sorted(arr[s:e + 1]):
+#             if i > k:
+#                 tempArr.append(i)
         
-        if len(tempArr) != 0:
-            answer.append(min(tempArr))
-        else:
-            answer.append(-1)
+#         if len(tempArr) != 0:
+#             answer.append(min(tempArr))
+#         else:
+#             answer.append(-1)
+
+        tempArr = list(filter(lambda x: x > k, arr[s:e + 1]))
+    
+        answer.append(min(tempArr) if tempArr else -1)
             
     return answer
