2ad28fc30a9b7d28fe557ba6b6d3534c3689f3d0
Seunghyun Kim <sevencuter@gmail.com>
2024-03-21 17:37:21 +0900

    수열과 구간 쿼리

---
+++ Level 0/Javascript/수열과 구간 쿼리1.js
@@ -0,0 +1,11 @@
+function solution(arr, queries) {
+  for (let i = 0; i < queries.length; i++) {
+    const [s, e] = queries[i];
+
+    for (let j = s; j <= e; j++) {
+      arr[j]++;
+    }
+  }
+
+  return arr;
+}
