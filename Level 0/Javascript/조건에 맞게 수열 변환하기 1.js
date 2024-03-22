9994cff2bc21926fa7cd22be4b5add700563ab17
Seunghyun Kim <sevencuter@gmail.com>
2024-03-22 13:28:25 +0900

    조건에 맞게 수열 변환하기 1

--- Level 0/Javascript/조건에 맞게 수열 변환하기 1.js
+++ Level 0/Javascript/조건에 맞게 수열 변환하기 1.js
@@ -1,29 +1,13 @@
 function solution(arr) {
-  let cnt = 0;
-  let prevArr = arr;
-
-  while (true) {
-    const currentArr = prevArr.map((v) => {
-      if (v >= 50 && v % 2 === 0) {
-        return parseInt(v / 2);
-      }
-
-      if (v < 50 && v % 2 !== 0) {
-        return v * 2 + 1;
-      }
-
-      return v;
-    });
-
-    const isSame = prevArr.every((v, i) => v === currentArr[i]);
-
-    if (isSame) {
-      break;
+  return arr.map((v) => {
+    if (v >= 50 && v % 2 === 0) {
+      return parseInt(v / 2);
     }
 
-    cnt += 1;
-    prevArr = currentArr;
-  }
+    if (v < 50 && v % 2 !== 0) {
+      return v * 2;
+    }
 
-  return cnt;
+    return v;
+  });
 }
