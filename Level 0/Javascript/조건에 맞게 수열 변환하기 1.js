cee3b4b499beb66463af74f971f125f7953645a5
Seunghyun Kim <sevencuter@gmail.com>
2024-03-22 11:13:40 +0900

    조건에 맞게 수열 변환하기 1

---
+++ Level 0/Javascript/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+  return arr.map((v) => {
+    if (v >= 50 && v % 2 === 0) {
+      return parseInt(v / 2);
+    }
+
+    if (v < 50 && v % 2 !== 0) {
+      return v * 2;
+    }
+
+    return v;
+  });
+}
