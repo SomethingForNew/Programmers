58c12f0f319228d1de275e658ce89aef2c2743dc
Seunghyun Kim <sevencuter@gmail.com>
2024-03-29 18:13:20 +0900

    TODO 추가

--- Level 0/Javascript/입문/분수의 덧셈.js
+++ Level 0/Javascript/입문/분수의 덧셈.js
@@ -1,5 +1,6 @@
 function solution(numer1, denom1, numer2, denom2) {
   // 최대공약수 구하기(유클리드 호제법)를 이용한 방법
+  // TODO: 유클리드 호제법을 사용 하지 않고 그냥 구현 가능한 방법 생각해보기
   const numerator = numer1 * denom2 + numer2 * denom1;
   const denominator = denom1 * denom2;
   const getGCD = (a, b) => (a % b ? getGCD(b, a % b) : b);
