a794bfb6da181dd4e37ac8a0ffc181531d0a65da
kimsh <kimsh@scggrm.co.kr>
2025-02-24 16:07:30 +0900

    Programmers Level2 조건에 부합하는 중고거래 상태 조회하기.sql

--- Level 2/SQL/Oracle/조건에 부합하는 중고거래 상태 조회하기.sql
+++ Level 2/SQL/Oracle/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,14 +1,13 @@
 -- 조건에 부합하는 중고거래 상태 조회하기.sql
-SELECT
-         A.BOARD_ID
-       , A.WRITER_ID
-       , A.TITLE
-       , A.PRICE
-       , CASE A.STATUS
-         WHEN 'SALE' THEN '판매중'
-         WHEN 'RESERVED' THEN '예약중'
-         WHEN 'DONE' THEN '거래완료'
-          END AS STATUS
-  FROM USED_GOODS_BOARD A
- WHERE A.CREATED_DATE = TO_DATE('2022-10-05', 'YYYY-MM-DD')
- ORDER BY A.BOARD_ID DESC
+SELECT BOARD_ID
+     , WRITER_ID
+     , TITLE
+     , PRICE
+     , CASE STATUS WHEN 'SALE' THEN '판매중'
+                   WHEN 'RESERVED' THEN '예약중'
+                   WHEN 'DONE' THEN '거래완료'
+        END AS STATUS
+    -- , DECODE(STATUS, 'SALE', '판매중', 'RESERVED', '예약중', 'DONE', '거래완료') AS STATUS
+  FROM USED_GOODS_BOARD
+ WHERE TO_CHAR(CREATED_DATE, 'YYYYMMDD') = '20221005'
+ ORDER BY BOARD_ID DESC
